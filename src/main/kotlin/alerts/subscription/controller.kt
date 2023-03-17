ab1009079ba7c37e62531c155b047d170b490c78
David Vega Lichacz <7826728+realdavidvega@users.noreply.github.com>
2023-03-17 11:06:46 +0100

    config: enable openapi with basic docs

--- src/main/kotlin/alerts/subscription/controller.kt
+++ src/main/kotlin/alerts/subscription/controller.kt
@@ -3,39 +3,53 @@ package alerts.subscription
 import alerts.user.SlackUserId
 import arrow.core.merge
 import arrow.core.raise.either
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import kotlinx.datetime.Clock
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
 import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatus.BAD_REQUEST
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 interface SubscriptionController {
   @GetMapping("/subscription")
+  @ApiResponses(value = [
+    ApiResponse(responseCode = "200", description = "List subscriptions"),
+    ApiResponse(responseCode = "400", description = "Slack user not found")
+  ])
   suspend fun get(
     @RequestParam("slackUserId") slackUserId: String
   ): ResponseEntity<Subscriptions>
 
   @PostMapping("/subscription")
+  @ApiResponses(value = [
+    ApiResponse(responseCode = "201", description = "Subscription created"),
+    ApiResponse(responseCode = "400", description = "Slack user or repository not found")
+  ])
   suspend fun post(
     @RequestParam("slackUserId") slackUserId: String,
     @RequestBody repository: Repository
   ): ResponseEntity<*>
 
   @DeleteMapping("/subscription")
+  @ApiResponses(value = [
+    ApiResponse(responseCode = "204", description = "Subscription deleted"),
+    ApiResponse(responseCode = "404", description = "Slack user not found")
+  ])
   suspend fun delete(
     @RequestParam("slackUserId") slackUserId: String,
     @RequestBody repository: Repository
   ): ResponseEntity<Nothing>
 }
 
-@Controller
+@RestController
 class DefaultSubscriptionController(
   private val service: SubscriptionService,
   private val clock: Clock,
